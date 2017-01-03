6cf5738456a234073147a7394030ba0edf5dbfc0
Alex Francois and Tracie Weitzman <alex@>
2017-01-03 13:47:30 -0800

    add humidity

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,14 @@
-var apiKey = "8a8261f1d3d4a1ab505d37111aa05d7f";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      console.log(JSON.stringify(response));
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%" + ".");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
   });
 });
