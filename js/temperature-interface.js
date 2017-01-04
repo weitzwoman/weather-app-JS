a45a6ac425d2f888c9c60e480f9f51c1487dd245
Alex Francois and Tracie Weitzman <tracie@>
2017-01-03 16:07:49 -0800

    add buttons to toggle between farenheit and celsius

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -6,14 +6,11 @@ $(document).ready(function(){
     var city = $('#location').val();
     $('#location').val("");
     $('.showCelsiusButton').show();
-
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       var kelvin = parseInt(response.main.temp);
       var newTemp = new Temp(kelvin);
       var farenheit = newTemp.convertFarenheit();
-
       $('.showWeather').text("The temperature in " + city + " is " + farenheit + " degrees(F).");
-
     }).fail(function(error) {
       $('.showWeather').text(error.responseJSON.message);
     });
@@ -22,8 +19,19 @@ $(document).ready(function(){
         var kelvin = parseInt(response.main.temp);
         var newTemp = new Temp(kelvin);
         var celsius = newTemp.convertCelsius();
-        console.log(celsius);
         $('.showWeather').empty().text("The temperature in " + city + " is " + celsius + " degrees(C).");
+        $('.showFarenheitButton').show();
+        $('.showCelsiusButton').hide();
+      });
+    });
+    $('#farenheit').click(function() {
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+        var kelvin = parseInt(response.main.temp);
+        var newTemp = new Temp(kelvin);
+        var farenheit = newTemp.convertFarenheit();
+        $('.showWeather').empty().text("The temperature in " + city + " is " + farenheit + " degrees(F).");
+        $('.showFarenheitButton').hide();
+        $('.showCelsiusButton').show();
       });
     });
   });
